6fa48ce1f799d13d03486c153a9b57aba14f8924
Eduard Ionel STAN <stan.i.eduard@gmail.com>
2026-08-20 16:43:26 +0200

    Make JET and reference ignores effective

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,9 @@ using Aletheia
 
 @testset "Aletheia" begin
     Aqua.test_all(Aletheia)
-    JET.report_package(Aletheia)
+    if pkgversion(JET) < v"0.11"
+        JET.test_package(Aletheia; target_defined_modules=true)
+    else
+        JET.test_package(Aletheia; target_modules=(Aletheia,), analyze_from_definitions=true)
+    end
 end
