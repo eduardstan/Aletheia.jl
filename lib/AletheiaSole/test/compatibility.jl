bee970bf0ceca023e8dcc8afa16fb64da6b320a4
Eduard Ionel STAN <stan.i.eduard@gmail.com>
2026-09-05 16:40:29 +0200

    Construct the SoleData condition with its feature type

--- lib/AletheiaSole/test/compatibility.jl
+++ lib/AletheiaSole/test/compatibility.jl
@@ -587,7 +587,7 @@ end
         @eval using AletheiaData
         logiset = SoleData.scalarlogiset([[1.0; 2.0], [3.0; 4.0]])
         prepared = AletheiaData.prepare_scalar(logiset)
-        condition = SoleData.ScalarCondition(1, >, 1.5)
+        condition = SoleData.ScalarCondition(SoleData.VariableValue(1), >, 1.5)
         @test AletheiaData.scalar_check(condition, prepared, 1, 1) === false
         @test AletheiaData.scalar_check(condition, prepared, 1, 2) === true
     end
