b6083f58482b767b838735de65bb00ee32a1cd57
Eduard Ionel STAN <stan.i.eduard@gmail.com>
2026-09-05 16:34:25 +0200

    Use a supported SoleData scalar fixture

--- lib/AletheiaSole/test/compatibility.jl
+++ lib/AletheiaSole/test/compatibility.jl
@@ -585,7 +585,7 @@ end
     if Base.find_package("SoleData") !== nothing
         @eval using SoleData
         @eval using AletheiaData
-        logiset = SoleData.scalarlogiset([1.0 2.0; 3.0 4.0])
+        logiset = SoleData.scalarlogiset([[1.0, 2.0], [3.0, 4.0]])
         prepared = AletheiaData.prepare_scalar(logiset)
         condition = SoleData.ScalarCondition(1, >, 1.5)
         @test AletheiaData.scalar_check(condition, prepared, 1, 1) === false
